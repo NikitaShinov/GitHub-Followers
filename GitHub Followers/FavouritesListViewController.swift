72ce4a5ec7383a3344c41ef82a7e8721b1042035
Nikita Shinov <shinov_96@mail.ru>
2022-02-09 02:38:27 +0300

    setup assets and scene delegate

---
+++ GitHub Followers/FavouritesListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavouritesListViewController.swift
+//  GitHub Followers
+//
+//  Created by max on 09.02.2022.
+//
+
+import UIKit
+
+class FavouritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+}
--- GitHub Followers/SearchViewController.swift
+++ GitHub Followers/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  GitHub Followers
 //
 //  Created by max on 09.02.2022.
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemPurple
+        
     }
+    
 
 
 }
-
