1a006cc93e897347166f5bd68c1c495800d5233f
Nikita Shinov <shinov_96@mail.ru>
2022-02-09 18:59:22 +0300

    create models

---
+++ GitHub Followers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHub Followers
+//
+//  Created by max on 09.02.2022.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
